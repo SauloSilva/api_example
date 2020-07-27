1cca0d17ee6a74ff9535996c014860e465fb16ae
Saulo Santiago <saulodasilvasantiago@gmail.com>
2020-07-27 02:30:43 -0300

    Improvements of seeds and locales of models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 require 'faker'
 
 products_attrs = 3.times.map { FactoryBot.attributes_for(:product) }
@@ -14,10 +6,8 @@ Product.create(products_attrs)
 stores_attrs = 3.times.map { FactoryBot.attributes_for(:store) }
 Store.create(stores_attrs)
 
-stock_items_atttr = [
+StockItem.create([
                       { product: Product.order('RANDOM()').first, store: Store.order('RANDOM()').first, quantity: rand(0..1000) },
                       { product: Product.order('RANDOM()').first, store: Store.order('RANDOM()').first, quantity: rand(0..1000) },
                       { product: Product.order('RANDOM()').first, store: Store.order('RANDOM()').first, quantity: rand(0..1000) }
-                    ]
-
-StockItem.create(stock_items_atttr)
+                    ])
